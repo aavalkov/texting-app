ac5a4a53ad17d2213e6c75c68c100f7243e61d11
Anna Valkov & lee kebler <student@epicodus.com>
2014-09-15 11:46:45 -0700

    initial commit and builds a systems that lets you text another phone via sms and twillio... yea we forgot to commit earlier.

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(messages_params)
+    if @message.save
+      redirect_to messages_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+private
+  def messages_params
+    params.require(:message).permit(:to, :from, :body)
+  end
+
+
+end
---
+++ app/models/message.rb
@@ -0,0 +1,17 @@
+class Message < ActiveRecord::Base
+  before_create :send_message
+
+private
+
+  def send_message
+    response = RestClient::Request.new(
+      :method => :post,
+      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC6b343ebc959e76f22f4866ce5c283c1a/Messages.json',
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
+      :payload => { :Body => body,
+                    :To => to,
+                    :From => from }
+      ).execute
+  end
+end
